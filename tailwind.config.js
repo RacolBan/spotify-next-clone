b2d15827cb80ed6ff891d4b88cb74e37585c0fb4
unknown <kain.199x@gmail.com>
2023-05-17 23:24:50 +0700

    initial first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,58 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      center: true,
+    },
     extend: {
+      screens: {
+        'msm': {'max': '425px'},
+        'mmd': {'max': '767px'},
+        'mlg': {'max': '1024px'},
+        'mxl': {'max': '1280px'},
+        'm2xl': {'max': '1440px'},
+        sm: '425px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1440px'
+      },
+      spacing: {
+        '1': '8px',
+        '2': '12px',
+        '3': '16px',
+        '4': '24px',
+        '5': '32px',
+        '6': '48px',
+      },
+      fontSize: {
+        xs: '0.6rem',
+        sm: '1rem',
+        xl: '1.5rem',
+        '2xl': '1.563rem',
+        '3xl': '1.953rem',
+        '4xl': '2.441rem',
+        '5xl': '3.052rem',
+      },
+      colors: {
+        vuejs: {
+          100: "#14a105",
+          200: "#18c206"
+        }
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+
   },
   plugins: [],
 }
