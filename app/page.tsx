9febe2dc10079cbed67df200b9cdf4bc8f6cfbcd
unknown <kain.199x@gmail.com>
2023-05-18 09:55:45 +0700

    done conflict

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import Center from '@/components/Center';
+import Center from 'components/Center';
 import Sidebar from 'components/Sidebar';
 
 export default function Home() {
   return (
    <div className='m-2 p-4'>
-    <h1>Hello world</h1>
+    <Sidebar /> 
+    <Center />  
    </div>
   )
 }
